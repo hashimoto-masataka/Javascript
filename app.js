063d463d81474600a8039bfdf793cf74743c69c4
EC2 Default User <ec2-user@ip-172-31-29-25.ap-northeast-1.compute.internal>
2023-02-25 00:35:38 +0000

    練習

---
+++ app.js
@@ -0,0 +1,10 @@
+let orange = 1000 ;
+let apple = 120 ;
+
+if(orange < apple){
+    alert('みかんの値段がリンゴより安い');
+} else if(orange== apple){
+    alert('みかんとりんごの値段が同じ');
+} else {
+    alert('みかんの値段がりんごより高い');
+}
